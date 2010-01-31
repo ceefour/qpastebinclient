28036b4ce243a161be2980ca2cb07653088faf6a
Hendy Irawan <hendy@soluvas.com>
2010-01-31 15:27:43 +0700

    license to LGPL 2.1+

--- qpastebinclient.h
+++ qpastebinclient.h
@@ -4,7 +4,7 @@ Copyright 2010 Soluvas <http://www.soluvas.com>
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
-version 3, or (at your option) any later version.
+version 2.1, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
